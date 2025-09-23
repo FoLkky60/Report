af4e9a4322d897835d422762a63c3606077b2be2
FoLkky60 <111549090+FoLkky60@users.noreply.github.com>
2025-09-23 10:05:49 +0700

    ขอความทตองการสำหรบ commit

--- WebApplication2/Default.aspx.cs
+++ WebApplication2/Default.aspx.cs
@@ -24,51 +24,44 @@ namespace WebApplication2
         {
             if (this.rbBtnListTypeReport.SelectedValue == "1") // ประวัติเครื่องจักรและอุปกรณ์
             {
-               //ReportMachineHository();
+                Report1();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "2") // บัญชีรายชื่อเครื่องมือเครื่องจักร (CI-F-PD-01)
             {
                 ReportMachineList();
             }
-            else if (this.rbBtnListTypeReport.SelectedValue == "3") // การบำรุงรักษาเครื่องจักร (CI-F-PD-08)
-            {
-                ReportMachineList();
-            }
-            else if (this.rbBtnListTypeReport.SelectedValue == "4") // แผนการบำรุงเครื่องจักร (CI-F-PD-07)
-            {
-                ReportMachineList();
-            }
+
             else if (this.rbBtnListTypeReport.SelectedValue == "5") // ตัดเครื่องจักรออกจากทะเบียนประวัติ (CI-F-PD-06)
             {
-                ReportMachineList();
+                Report5();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "6") // การตรวจสอบดูแลเครื่องมือเครื่องจักรประจำวัน Daily Check Sheet (CI-F-PD-03)
             {
-                Report1();
+                Report6();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "7") // การตรวจสอบดูแลเครื่องมือเครื่องจักรประจำวัน Daily Check Sheet (CI-F-PD-03)
             {
-                ReportMachineList();
+                Report7();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "8") // การตรวจสอบดูแลเครื่องมือเครื่องจักรประจำวัน Daily Check Sheet (CI-F-PD-03)
             {
-                ReportMachineList();
+                Report8();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "9") // การตรวจสอบดูแลเครื่องมือเครื่องจักรประจำวัน Daily Check Sheet (CI-F-PD-03)
             {
-                ReportMachineList();
+                Report9();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "10") // การตรวจสอบดูแลเครื่องมือเครื่องจักรประจำวัน Daily Check Sheet (CI-F-PD-03)
             {
-                ReportMachineList();
+                Report10();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "11") // การตรวจสอบดูแลเครื่องมือเครื่องจักรประจำวัน Daily Check Sheet (CI-F-PD-03)
             {
-                ReportMachineList();
+                Report11();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "12") // การตรวจสอบดูแลเครื่องมือเครื่องจักรประจำวัน Daily Check Sheet (CI-F-PD-03)
             {
-                ReportMachineList();
+                Report12();
             }
             else if (this.rbBtnListTypeReport.SelectedValue == "WI") // WI วิธีการปฎิบัติงาน Work Instruction (CI-F-PD-09)
             {
@@ -89,12 +82,84 @@ namespace WebApplication2
         private void Report1()
         {
             string pageUrl = "view_report.aspx?";
+            string pMachineType = "1";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+        private void Report5()
+        {
+            string pageUrl = "view_report.aspx?";
+            string pMachineType = "5";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+        private void Report6()
+        {
+            string pageUrl = "view_report.aspx?";
             string pMachineType = "6";
             string pDept = "";
             string pMachineCode = "";
             string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
             ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
         }
-
+        private void Report7()
+        {
+            string pageUrl = "view_report.aspx?";
+            string pMachineType = "7";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+        private void Report8()
+        {
+            string pageUrl = "view_report.aspx?";
+            string pMachineType = "8";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+        private void Report9()
+        {
+            string pageUrl = "view_report.aspx?";
+            string pMachineType = "9";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+        private void Report10()
+        {
+            string pageUrl = "view_report.aspx?";
+            string pMachineType = "10";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+        private void Report11()
+        {
+            string pageUrl = "view_report.aspx?";
+            string pMachineType = "11";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+        private void Report12()
+        {
+            string pageUrl = "view_report.aspx?";
+            string pMachineType = "12";
+            string pDept = "";
+            string pMachineCode = "";
+            string parameter = pageUrl + "pMachineType=" + pMachineType + "&pMachineCode=" + pMachineCode + "&pDept=" + pDept;
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Report", String.Format("window.open({0});", "'" + parameter + "'"), true);
+        }
+      
     }
 }
--- WebApplication2/view_report.aspx.cs
+++ WebApplication2/view_report.aspx.cs
@@ -22,11 +22,73 @@ namespace WebApplication2
 
         private void getDateReportMachineListCI()
         {
-            crpt.Load(Server.MapPath("/Report/MachineList.rpt"));
-            deleteFolderLast();
-            ConvertToPDF_preview();
+        
+            string machineTypesParam = Request.QueryString["pMachineType"];
+
+            if (!string.IsNullOrEmpty(machineTypesParam))
+            {
+            
+                string[] reportTypes = machineTypesParam.Split(',');
+
+        
+                foreach (string reportType in reportTypes)
+                {
+                    ReportDocument crpt = new ReportDocument();
+
+            
+                    if (reportType == "1")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report1.rpt"));
+                    }
+                    else if (reportType == "2")
+                    {
+                        crpt.Load(Server.MapPath("/Report/MachineList.rpt"));
+                    }
+                    else if (reportType == "5")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report5.rpt"));
+                    }
+                    else if (reportType == "6")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report6.rpt"));
+                    }
+                    else if (reportType == "7")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report7.rpt"));
+                    }
+                    else if (reportType == "8")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report8.rpt"));
+                    }
+                    else if (reportType == "9")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report9.rpt"));
+                    }
+                    else if (reportType == "10")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report10.rpt"));
+                    }
+                    else if (reportType == "11")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report11.rpt"));
+                    }
+                    else if (reportType == "12")
+                    {
+                        crpt.Load(Server.MapPath("/Report/Report12.rpt"));
+                    }
+                    
+
+                    deleteFolderLast();
+                    ConvertToPDF_preview(crpt); 
+                }
+            }
+            else
+            {
+                // ถ้าไม่มีค่าพารามิเตอร์ pMachineType ส่งมาใน URL
+                Response.Write("No report types selected.");
+            }
         }
-        private void ConvertToPDF_preview()
+        private void ConvertToPDF_preview(ReportDocument crpt)
         {
             // Export File To PDF
             string sysDateCurrent = DateTime.Now.ToString("yyyyMMdd");
